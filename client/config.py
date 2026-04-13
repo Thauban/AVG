fc39559d6e91ee9f68c1eae68e0b83034ac92238
unknown <coda1014@h-ka.de>
2026-04-13 19:08:59 +0200

    refactor: Aktualisiere Projektstruktur und Konfiguration

--- client/config.py
+++ client/config.py
@@ -6,13 +6,17 @@ sowie die Standardwaehrung fuer Zahlungsnachrichten zentral verwaltet.
 """
 
 import os
+from dotenv import load_dotenv
+
+# Lade Umgebungsvariablen aus .env Datei (falls vorhanden)
+load_dotenv()
 
 GRPC_SERVER = os.getenv("GRPC_SERVER", "localhost:50051")
 
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "localhost")
 RABBITMQ_PORT = int(os.getenv("RABBITMQ_PORT", 5672))
-RABBITMQ_USER = os.getenv("RABBITMQ_USER", "admin")
-RABBITMQ_PASS = os.getenv("RABBITMQ_PASS", "avg123")
+RABBITMQ_USER = os.getenv("RABBITMQ_USER")
+RABBITMQ_PASS = os.getenv("RABBITMQ_PASS")
 RABBITMQ_QUEUE = os.getenv("RABBITMQ_QUEUE", "payment_queue")
 
 DEFAULT_CURRENCY = "EUR"
